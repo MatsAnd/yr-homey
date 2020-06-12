3825b2f56bc8337e94a08268c317cacce352835e
Mats Andreassen <matsand@live.no>
2020-06-12 22:45:58 +0200

    Added trigger registration

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 'use strict'
 
 const Homey = require('homey')
+const app = require('./app.json')
 
 class Yr extends Homey.App {
   onInit () {
-    this.log('Yr is running...')
+    this.log(`${app.name.en} version ${app.version} is running...`)
+
+    // Register flow cards
+    this.registerTriggers()
+    this.registerConditions()
+  }
+
+  registerTriggers () {
+    this.log('Registering triggers...')
+    new Homey.FlowCardTrigger('starts_to_rain').register()
+  }
+
+  registerConditions (conditions) {
+    this.log(`Registering ${conditions ? conditions.length : 'none'} conditions!`)
+
+    conditions.forEach(condition => {
+      this.log(`Registering condition ${condition}...`)
+      new Homey.FlowCardCondition(condition).register()
+      this.log(`Registered condition ${condition}!`)
+    })
   }
 }
 
